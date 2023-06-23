dbff3bcbeecb583314fa89bf2cda8418d2a592ff
Perumallpardhiv <lit2021001@iiitl.ac.in>
2023-06-23 16:47:57 +0530

    connected mongodb atlas

--- config/db.js
+++ config/db.js
@@ -1,9 +1,16 @@
 const mongoose = require('mongoose');
 
-const connection = mongoose.createConnection('mongodb://127.0.0.1:27017/imartBackend').on('open',()=>{
+const connection = mongoose.createConnection('mongodb+srv://imart:y8ChACjDpndJF5JB@cluster0.7o4islm.mongodb.net/?retryWrites=true&w=majority').on('open',()=>{
     console.log("MongoDb Connected");
 }).on('error',()=>{
     console.log("MongoDb Connection error");
 });
+
+// const connection = mongoose.connect("mongodb+srv://imart:y8ChACjDpndJF5JB@cluster0.7o4islm.mongodb.net/?retryWrites=true&w=majority", {
+//     useNewUrlParser : true,
+//     useUnifiedTopology : true
+//   })
+//   .then(console.log("Connected to MongoDB"))
+//   .catch(err => console.log(err))
 
 module.exports = connection;
