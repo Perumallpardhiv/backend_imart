c2576370834dfda998a0f498e076d4cfc81044c9
Perumallpardhiv <lit2021001@iiitl.ac.in>
2023-08-08 18:44:18 +0530

    route update

--- routes/user_route.js
+++ routes/user_route.js
@@ -2,7 +2,7 @@ const router = require("express").Router();
 const UserController = require('../controller/user_controller');
 
 router.post('/registration',UserController.register);
-router.post('/sendOtp',UserController.login);
+router.post('/login',UserController.login);
 router.get('/allUsers',UserController.getUsers);
 
 module.exports = router;
