1eb53e87c76ad0edb4cbe2e29cc04a839a7dfa49
Perumallpardhiv <lit2021001@iiitl.ac.in>
2023-08-10 15:32:40 +0530

    admin send otp verify otp

--- controller/admin_controller.js
+++ controller/admin_controller.js
@@ -1,4 +1,9 @@
 const AdminService = require('../services/admin_service');
+const accountSid = "AC5a22ae6c3642b3ca8b283717ac82eb4e";
+require('dotenv').config();
+const authToken = process.env.TWILIO_AUTH_TOKEN;
+const verifySid = "VA1d4afff47414d72f227186a2288cb26e";
+const client = require('twilio')(accountSid, authToken);
 
 exports.register = async (req, res, next) => {
     try {
@@ -7,7 +12,9 @@ exports.register = async (req, res, next) => {
 
         if (!admin) {
             const successReg = await AdminService.registerAdmin(mobileno, device_token, name);
-            res.json({ status: 1, message: "Admin Registered Successfully" });
+            let tokenData = { _id: admin._id, mobileno: admin.mobileno, name: admin.name };
+            const token = await AdminService.generateToken(tokenData, "secretKey", '24h');
+            res.json({ status: 1, message: "Admin Registered Successfully", token: token});
             console.log("Registered Successfully");
         } else {
             res.json({ status: 0, message: "User Already exist" });
@@ -37,5 +44,39 @@ exports.login = async (req, res, next) => {
         res.json({ status: 0, message: "Logging Failed" });
         console.log("Some Error Logging Failed");
         throw er
+    }
+}
+
+exports.sendOtpadmin = async (req, res, next) => {
+    const { phoneNumber } = req.body;
+    try {
+        client.verify.v2
+        .services(verifySid)
+        .verifications.create({ to: phoneNumber, channel: "sms" })
+        .then((verification) => {
+            console.log(`ver: ${verification.status}`);
+            res.json({ status: 1, message: "Otp sent"});
+            console.log("Otp sent");
+        })
+    } catch (er) {
+        res.json({ status: 0, message: er });
+        console.log("Otp Sending Failed");
+    }
+}
+
+exports.verifyOtpadmin = async (req, res, next) => {
+    const { phoneNumber, otpCode } = req.body;
+    try {
+        client.verify.v2
+        .services(verifySid)
+        .verificationChecks.create({ to: phoneNumber, code: otpCode })
+        .then((verification_check) => {
+            console.log(`ver: ${verification_check.status}`);
+            res.json({ status: 1, message: "Otp verified"});
+            console.log("Otp verified");
+        })
+    } catch (er) {
+        res.json({ status: 0, message: "Otp Wrong"});
+        console.log("Otp Wrong");
     }
 }
--- routes/admin_route.js
+++ routes/admin_route.js
@@ -3,5 +3,7 @@ const AdminController = require('../controller/admin_controller');
 
 router.post('/adminRegister',AdminController.register);
 router.post('/adminLogin',AdminController.login);
+router.post('/sendOtpadmin',AdminController.sendOtpadmin);
+router.post('/verifyOtpadmin',AdminController.verifyOtpadmin);
 
 module.exports = router;
